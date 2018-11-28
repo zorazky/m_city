b3c81011ead34cbcd9c36b598f481057d702431b
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-27 22:22:31 -0600

    header and footer created

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import { CityLogo } from '../ui/icons';
 
 
 class Header extends Component {
@@ -17,10 +18,15 @@ class Header extends Component {
                     borderBottom: '2px solid #00285e'
                 }}
             >
-                <Toolbar styles={{display:'flex'}}>
-                    <div styles={{flexGrow: 1}}>
+                <Toolbar style={{display:'flex'}}>
+                    <div style={{flexGrow: 1}}>
                         <div className="header_logo">
-                            LOGO
+                            <CityLogo 
+                                link={true}
+                                linkTo="/"
+                                width="70px"
+                                height="70px"
+                            />
                         </div>
                     
                     </div>
