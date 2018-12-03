0a535fba40cf3321deb22d233945953fa21c88d0
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-02 21:02:46 -0600

    meet the players section started

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './Featured';
 import Matches from './Matches';
+import MeetPlayers from './MeetPlayers';
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured />
             <Matches />
+            <MeetPlayers />
         </div>
     )
 }
