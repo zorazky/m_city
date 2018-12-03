1ad40b8ed6196823cc62d50887d3f46dbb227774
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-03 14:50:25 -0600

    email input started

--- src/Components/Home/Promotion/Enroll.js
+++ src/Components/Home/Promotion/Enroll.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import FormField from '../../ui/formFields';
 
-const PromotionAnimation = () => {
-    return (
-        <div>
-            animation
-        </div>
-    )
+class Enroll extends Component {
+
+    state = {
+        formError:false,
+        formSuccess:'',
+        formdata: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email:true
+                },
+                valid: false,
+                validationMessage: ''
+            }
+        }
+    }
+    submitForm() {
+
+    }
+    render() {
+        return (
+            <Fade>
+                <div className="enroll_wrapper">
+                    <form onSubmit={(event) => this.submitForm(event)}>
+                        <div className="enroll_title">Enter your email</div>
+                        <div className="enroll_input">
+                            <FormField 
+                                id={'email'}
+                                formdata={this.state.formdata.email}
+                                
+                            />
+                        </div>
+                    </form>
+                </div>
+            </Fade>
+        )
+    }
 }
 
-export default PromotionAnimation;
+export default Enroll;
