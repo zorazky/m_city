4b5d6243ee1ebb383c5f26fdbaef995d4b8df669
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-04 12:49:58 -0600

    working on validation

--- src/Components/Home/Promotion/Enroll.js
+++ src/Components/Home/Promotion/Enroll.js
@@ -41,11 +41,30 @@ class Enroll extends Component {
         newFormdata[element.id] = newElement;
 
         this.setState({
+            formError: false,
             formdata: newFormdata
         })
     }
-    submitForm() {
+    submitForm(event) {
+        event.preventDefault();
 
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for(let key in this.state.formdata) {
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+
+        }
+
+        if (formIsValid) {
+            console.log(dataToSubmit);
+        } else {
+            this.setState({
+                formError: true
+            })
+        }
+        
     }
     render() {
         return (
@@ -60,6 +79,8 @@ class Enroll extends Component {
                                 change={(element) => this.updateForm(element)}
                                 
                             />
+                            { this.state.formError ? <div className="error_label">Something is wrong, try again</div>:null}
+                            <button onClick={(event) => this.submitForm(event)}>Enroll</button>
                         </div>
                     </form>
                 </div>
