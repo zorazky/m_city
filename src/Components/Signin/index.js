34c2a34852aaf8665ff06dcba2b9719ecabc6caa
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-05 13:42:13 -0600

    login started, routes are not working

---
+++ src/Components/Signin/index.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import FormField from '../ui/formFields';
+import { validate } from '../ui/misc';
+
+
+
+class SignIn extends Component {
+    
+    state = {
+        formError:false,
+        formSuccess:'',
+        formdata: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email:true
+                },
+                valid: false,
+                validationMessage: ''
+            },
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'password_input',
+                    type: 'password',
+                    placeholder: 'Enter your password'
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                validationMessage: ''
+            }
+        }
+    }
+
+    submitForm(event) {
+        event.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for(let key in this.state.formdata) {
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+
+        }
+
+        if (formIsValid) {
+            console.log(dataToSubmit);
+        } else {
+            this.setState({
+                formError: true
+            })
+        }
+    }
+    updateForm(element) {
+        const newFormdata = {...this.state.formdata}
+        const newElement = {...newFormdata[element.id]}
+
+        newElement.value = element.event.target.value;
+
+        let validData = validate(newElement)
+
+        newElement.valid = validData[0];
+        newElement.validationMessage = validData[1];
+
+    
+        newFormdata[element.id] = newElement;
+
+        this.setState({
+            formError: false,
+            formdata: newFormdata
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <div className="signin_wrapper" style={{margin:'100px'}}>
+                    <form onSubmit={(event) => this.submitForm(event)}>
+                        <h2>Login</h2>
+                        <FormField 
+                                id={'email'}
+                                formdata={this.state.formdata.email}
+                                change={(element) => this.updateForm(element)}
+                                
+                            />
+
+                        <FormField 
+                            id={'password'}
+                            formdata={this.state.formdata.password}
+                            change={(element) => this.updateForm(element)}
+                            
+                        />
+                        <button onClick={(event) => this.submitForm(event)}>Log in</button>
+                    </form>
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignIn;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './Components/Home';
+import SignIn from './Components/Signin';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
     </Layout>
