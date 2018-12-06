a832a2ada37eaf5c521f4a0f1df8bd35ce8ff332
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-06 09:43:28 -0600

    dashboard created

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Components/Home';
 import SignIn from './Components/Signin';
+import Dashboard from './Components/Admin/Dashboard';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={SignIn} path="/sign_in"/>
         <Route exact component={Home} path="/" />
       </Switch>
