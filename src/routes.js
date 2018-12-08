3becf3a938ae5d3f9a197b36e46ee3a8c75b3ec2
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-07 21:12:33 -0600

    matches created on dashboard

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,14 @@ import PublicRoute from './Components/AuthRoutes/publicRoutes';
 import Home from './Components/Home';
 import SignIn from './Components/Signin';
 import Dashboard from './Components/Admin/Dashboard';
+import AdminMatches from './Components/Admin/matches';
 
 const Routes = (props) => {
   
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn} />
         <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
