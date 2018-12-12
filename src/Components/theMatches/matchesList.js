bae3e47b387e337247be84e215ff79394b1fc834
Oswaldo <oswaldo.vazquez@gmail.com>
2018-12-12 14:48:00 -0600

    working on the matches page

--- src/Components/theMatches/matchesList.js
+++ src/Components/theMatches/matchesList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { easePolyOut } from 'd3-ease';
+import NodeGroup from 'react-move/NodeGroup';
 
 class MatchesList extends Component {
     state = {
@@ -10,9 +12,59 @@ class MatchesList extends Component {
             matcheslist: props.matches
         }
     }
+
+    showMatches = () => (
+        this.state.matcheslist ?
+            <NodeGroup
+                data={this.state.matcheslist}
+                keyAccessor={(d) => d.id}
+
+                start={() => ({
+                    opacity:0,
+                    x:-200
+                })}
+
+                enter={(d, i) => ({
+                    opacity:[1],
+                    x:[0],
+                    timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+                })}
+
+                update={(d, i) => ({
+                    opacity:[1],
+                    x:[0],
+                    timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+                })}
+
+                leave={(d, i) => ({
+                    opacity:[0],
+                    x:[-200],
+                    timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+                })}
+            >
+                {(nodes) => (
+                    <div>
+                        { nodes.map(({key, data, state:{x, opacity}}) => (
+                            <div key={key} className="match_box_big"
+                                style={{
+                                    opacity,
+                                    transform: `translate(${x}px)`
+                                }}
+                            >
+                                working
+                            </div>
+                        ))}
+                    </div>
+                )}
+            
+            </NodeGroup>
+        :null
+    )
     render() {
         return (
-            <div>list</div>
+            <div>
+                {this.showMatches()}
+            </div>
         )
     }
 }
